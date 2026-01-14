1320a3963a26bc291acd703f0704750d66208f5d
PhanThanhSum <phanthanhsumvn@gmail.com>
2026-01-14 15:09:28 +0700

    test comment

--- src/main/java/com/example/airlabproject/AirlabProjectApplication.java
+++ src/main/java/com/example/airlabproject/AirlabProjectApplication.java
@@ -1,11 +1,7 @@
 package com.example.airlabproject;
 
-import com.example.airlabproject.service.AirlineService;
-import com.example.airlabproject.service.AirportService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AirlabProjectApplication {
@@ -13,16 +9,8 @@ public class AirlabProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(AirlabProjectApplication.class, args);
     }
+    
 
-    @Bean
-    CommandLineRunner initDatabase(AirlineService airlineService) {
-        return args -> {
-            boolean airlineEmpty = airlineService.isDBEmpty();
 
-            if (airlineEmpty) {
-                System.out.println(" Fetching airlines from API...");
-                airlineService.AirlinesLoadDB();
-            }
-        };
-    }
+    //test comment
 }
